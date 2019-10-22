d035d3ffe82f5374c13bfc2cceac92012673efac
Tharun Reddy <sabbu.tharun@gmail.com>
2019-10-21 21:06:15 -0400

    Update K8s_Windows_Automation_Idea.ps1

--- K8s_Windows_Automation_Idea.ps1
+++ K8s_Windows_Automation_Idea.ps1
@@ -5,11 +5,13 @@
     $REGION=$REGION.Substring(0,$REGION.Length-1)
     $instanceTags = Get-EC2Tag -Region $REGION -Filter @{ Name="resource-id"; Values=$INSTANCE_ID }
     $ClusterName = $instanceTags.Where({$_.Key -eq $TAG_NAME}).Value
+    Write-Output "Started User-Data to join node to the cluster"
     while ($true)
     {
-      if(Get-S3Object -BucketName ${S3Bucket_NAME} | where{$_.Key -like "kubeadm/${ClusterName}"})
+      if(Get-S3Object -BucketName ${S3Bucket-Placeholder} "kubeadm" | where{$_.Key -like "kubeadm/${ClusterName}/config"})
       { 
-        aws.cmd s3 cp s3://${S3Bucket_NAME}/kubeadm/${ClusterName}/config C:\k\
+        Write-Output "Started process to join node"
+        aws.cmd s3 cp s3://${S3Bucket-Placeholder}/kubeadm/${ClusterName}/config C:\k\
         $env:HostIP = (
             Get-NetIPConfiguration |
             Where-Object {
@@ -17,13 +19,13 @@
                 $_.NetAdapter.Status -ne "Disconnected"
             }
         ).IPv4Address.IPAddress
-        C:\k\awsclisetup.ps1
         C:\k\join.ps1 -ManagementIP  $env:HostIP -NetworkMode overlay -ClusterCIDR 172.21.128.0/17 -KubeDnsServiceIP 172.21.0.10 -ServiceCIDR 172.21.0.0/17 -LogDir C:\k\logs -KubeletFeatureGates ""
-        C:\k\fix_metadata.ps1
+        Write-Output "Successfully joined to the cluster"
         Break
       } 
       else 
       { 
+        Write-Output "Waiting for the kubeconfig"
         Start-Sleep -Seconds 30 
       }
     }
